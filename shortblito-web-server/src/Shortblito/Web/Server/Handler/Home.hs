0ec5f8f1c052085065289e73dcbaf2195e9c182f
Pablo <pablo>
2021-06-10 18:57:20 +0200

    Make shortener that returns the id of the shortened url

--- shortblito-web-server/src/Shortblito/Web/Server/Handler/Home.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-
-module Shortblito.Web.Server.Handler.Home where
-
-import Shortblito.Web.Server.Handler.Import
-
-getHomeR :: Handler Html
-getHomeR = defaultLayout $(widgetFile "home")
