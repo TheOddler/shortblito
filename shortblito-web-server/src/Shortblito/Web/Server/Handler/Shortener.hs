3c36049bae9ca63185d24018ce4a8bcdccec3fdf
Pablo <pablo>
2021-06-10 19:30:40 +0200

    Don't require json to be send, simply read the body as a string for that is the url

--- shortblito-web-server/src/Shortblito/Web/Server/Handler/Shortener.hs
+++ shortblito-web-server/src/Shortblito/Web/Server/Handler/Shortener.hs
@@ -7,6 +7,10 @@
 module Shortblito.Web.Server.Handler.Shortener where
 
 import Control.Applicative ((<$>), (<*>))
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import qualified Data.Conduit.Text as CT
+import Data.Maybe (listToMaybe)
 import Data.Text
 import Database.Persist.Sql (fromSqlKey)
 import Shortblito.BaseChanging
@@ -15,19 +19,17 @@ import Shortblito.Web.Server.Foundation
 import Shortblito.Web.Server.Handler.Import
 import Yesod
 
-data LongUrl = LongUrl {url :: Text}
-
-instance FromJSON LongUrl where
-  parseJSON (Object v) = LongUrl <$> v .: "url"
-
 getShortenerR :: Handler Html
 getShortenerR = defaultLayout $(widgetFile "home")
 
 postShortenerR :: Handler Text
 postShortenerR = do
-  longUrl <- requireCheckJsonBody :: Handler LongUrl -- Foo -- get the json body as Foo (assumes FromJSON instance)
-  existingUrl <- runDB $ getBy $ UniqueLong $ url longUrl
-  key <- case existingUrl of
-    Just (Entity key _) -> pure key
-    Nothing -> do runDB $ insert Url {urlLong = url longUrl}
-  pure $ pack $ show $ toBase $ fromSqlKey key
+  lines <- rawRequestBody $$ CT.decode CT.utf8 =$ CL.consume -- Foo -- get the json body as Foo (assumes FromJSON instance)
+  case listToMaybe lines of
+    Just longUrl -> do
+      existingUrl <- runDB $ getBy $ UniqueLong longUrl
+      key <- case existingUrl of
+        Just (Entity key _) -> pure key
+        Nothing -> do runDB $ insert Url {urlLong = longUrl}
+      pure $ pack $ show $ toBase $ fromSqlKey key
+    Nothing -> invalidArgs []
