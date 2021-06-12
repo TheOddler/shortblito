06ab5453614c72268daabc8432ba490dae0839ba
Pablo <pablo>
2021-06-12 10:06:12 +0200

    Test getting shortened urls

--- shortblito-web-server/test/Shortblito/Web/Server/Handler/ElongatorSpec.hs
+++ shortblito-web-server/test/Shortblito/Web/Server/Handler/ElongatorSpec.hs
@@ -10,10 +10,17 @@ spec = shortblitoWebServerSpec $
     yit "can report invalid key" $ do
       get $ ElongatorR "$"
       statusIs 400
-    yit "can retrieve shortened url" $ do
+    yit "can retrieve a shortened url" $ do
       postBody ShortenerR "https://www.pabloproductions.be/"
       get $ ElongatorR "a"
       statusIs 301
     yit "can report not found" $ do
       get $ ElongatorR "a"
       statusIs 404
+    yit "can retrieve the correct shortened url" $ do
+      postBody ShortenerR "https://www.something.else"
+      postBody ShortenerR "https://wanted.url"
+      postBody ShortenerR "https://again.something.else"
+      get $ ElongatorR "b"
+      statusIs 301
+      assertHeader "Location" "https://wanted.url"
